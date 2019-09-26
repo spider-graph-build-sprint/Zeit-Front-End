41b4ab7451dc5da3df45bad08b2d1a89e0342f3a
doanmade <43007082+doanmade@users.noreply.github.com>
2019-09-26 16:42:17 -0700

    Got redux connected and moved forms to there own components for cleaner code structor

--- src/components/GraphCard.js
+++ src/components/GraphCard.js
@@ -1,13 +1,26 @@
 import React from "react";
-import {withRouter} from "react-router-dom";
+import { connect } from "react-redux";
+import { deleteGraph } from "../reducers/graphs/actions";
 
 const GraphCard = props => {
-    return (
-        <div onClick={() => props.history.push(props.path)} className="graphCard">
-            <p>{props.name}</p>
-            <img src={props.imgUrl} alt="radar graph"/>
-        </div>
-    );
-}
+  return (
+    <div onClick={() => props.history.push(props.path)} className="graphCard">
+      <p>{props.name}</p>
+      <img src={props.imgUrl} alt="radar graph" />
+      <button onClick={() => deleteGraph()}>X</button>
+    </div>
+  );
+};
 
-export default withRouter(GraphCard);
+const mapPropsToState = state => {
+  return {
+    isAuth: state.user.isAuth
+  };
+};
+
+export default connect(
+  mapPropsToState,
+  { deleteGraph }
+)(GraphCard);
+
+// export default withRouter(GraphCard);
