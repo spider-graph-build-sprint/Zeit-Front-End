bf70af503dad6b1ecd96e35c041c62c44eb6c453
lowell1 <lowelljacobs@mail.com>
2019-09-26 14:07:09 -0700

    started dataset form

---
+++ src/components/DataSetForm.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+const DataSetForm = props => {
+    return (
+        <form>
+            {
+                props.labels
+            }
+        </form>
+    );
+}
